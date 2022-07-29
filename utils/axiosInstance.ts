6417ac824c621614fd77b06af4c179b847163d52
Nikola Vukovic <nikolav54@gmail.com>
2022-07-29 14:12:19 +0200

    Added default timeout of 5 seconds on Axios instance

--- utils/axiosInstance.ts
+++ utils/axiosInstance.ts
@@ -4,4 +4,5 @@ export const axiosInstance: AxiosInstance = axios.create({
   baseURL: 'https://api.spacexdata.com/v4/',
   responseType: 'json',
   headers: { 'Content-type': 'application/json', Accept: 'application/json' },
+  timeout: 5000,
 });
